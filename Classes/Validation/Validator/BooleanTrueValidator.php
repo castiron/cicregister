4cf3a1b15ab489e1ab13c3d523b6889ae92e209c
Zach Davis <zach@castironcoding.com>
2012-01-25 10:42:56 -0800

    [FEATURE] Added BooleanTrue validator

---
+++ Classes/Validation/Validator/BooleanTrueValidator.php
@@ -0,0 +1,48 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2011 Zachary Davis <zach@castironcoding.com>, Cast Iron Coding, Inc
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ *
+ * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License, version 3 or later
+ *
+ */
+
+class Tx_Cicregister_Validation_Validator_BooleanTrueValidator extends Tx_Extbase_Validation_Validator_AbstractValidator {
+
+	/**
+	 * @param bool $value
+	 * @return bool
+	 */
+	public function isValid($value) {
+		if ($value === TRUE) {
+			return TRUE;
+		} else {
+			$error = new Tx_Extbase_Validation_Error('Boolean Value is not true', 1327519111);
+			$this->result->addError($error);
+			return FALSE;
+		}
+	}
+
+}
