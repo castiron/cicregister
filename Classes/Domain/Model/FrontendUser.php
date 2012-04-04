f855cb800954565c77b9840b57be0661252e489b
Peter Soots <peter@castironcoding.com>
2012-04-04 13:58:17 -0700

    [FEATURE] added a FrontendUser.getEmail method

--- Classes/Domain/Model/FrontendUser.php
+++ Classes/Domain/Model/FrontendUser.php
@@ -122,6 +122,14 @@ class Tx_Cicregister_Domain_Model_FrontendUser extends Tx_Extbase_Domain_Model_F
 
 	/**
 	 * username == email
+	 * @return string
+	 */
+	public function getEmail() {
+		return $this->email;
+	}
+
+	/**
+	 * username == email
 	 * @param $username
 	 */
 	public function setUsername($username) {
