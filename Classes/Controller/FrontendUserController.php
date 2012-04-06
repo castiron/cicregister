5e1a9b5c9fb37de2b36597733afa1c03e957009e
Gabe Blair <gabe@castironcoding.com>
2012-04-06 14:41:27 -0700

    [BUGFIX] Uncomment lines that assign group to user record in 'saveEnrollmentAction'

--- Classes/Controller/FrontendUserController.php
+++ Classes/Controller/FrontendUserController.php
@@ -131,12 +131,10 @@ class Tx_Cicregister_Controller_FrontendUserController extends Tx_Cicregister_Co
 		$this->handleBehaviorResponse($this->doBehaviors($frontendUser, 'updated', 'edit'), $frontendUser);
 	}
 
-	/*
+	/**
 	 * Show a form for a user to submit an enrollment code
-	 * @param Tx_Cicregister_Domain_Model_FrontendUser $frontendUser
 	 */
 	public function enrollAction() {
-		// TODO : output flash messages in the view
 		if(!$this->userIsAuthenticated) {
 			$this->forward('new');
 		}
@@ -153,8 +151,8 @@ class Tx_Cicregister_Controller_FrontendUserController extends Tx_Cicregister_Co
 			$group = $this->frontendUserGroupRepository->findOneByEnrollmentCode($enrollmentCode);
 			if($group instanceof Tx_Cicregister_Domain_Model_FrontendUserGroup) {
 				if($this->userIsAuthenticated) {
-#					$frontendUser = $this->frontendUserRepository->findByUid($this->userData['uid']);
-#					$frontendUser->addUserGroup($group);
+					$frontendUser = $this->frontendUserRepository->findByUid($this->userData['uid']);
+					$frontendUser->addUserGroup($group);
 					$this->flashMessageContainer->add('You have been enrolled');
 				} else {
 					$this->flashMessageContainer->add('You must be logged in to enroll yourself','',t3lib_FlashMessage::ERROR);
