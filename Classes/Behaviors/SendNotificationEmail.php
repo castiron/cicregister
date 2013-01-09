2916079e2b78693d3335fab16d4ef4fafeb615db
Peter Soots <peter@castironcoding.com>
2013-01-09 12:35:26 -0800

    [FEATURE] Added a new SendNotificationEmail Behavior.

---
+++ Classes/Behaviors/SendNotificationEmail.php
@@ -0,0 +1,39 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Zach Davis <zach@castironcoding.com>, Cast Iron Coding
+ *  Lucas Thurston <lucas@castironcoding.com>, Cast Iron Coding
+ *  Gabe Blair <gabe@castironcoding.com>, Cast Iron Coding
+ *  Peter Soots <peter@castironcoding.com>, Cast Iron Coding
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+class Tx_Cicregister_Behaviors_SendNotificationEmail extends Tx_Cicregister_Behaviors_AbstractBehavior implements Tx_Cicregister_Behaviors_BehaviorInterface {
+
+	public function execute(Tx_Cicregister_Domain_Model_FrontendUser $frontendUser, array $conf) {
+		$settings = $this->configurationManager->getConfiguration(Tx_Extbase_Configuration_ConfigurationManagerInterface::CONFIGURATION_TYPE_SETTINGS);
+		$sender[$conf['senderEmail']] = $conf['senderName'];
+		$receiver[$conf['receiverEmail']] = $conf['receiverName'];
+		$this->sendTemplateEmail($sender, $receiver, $conf['subject'], $conf['template'], array('frontendUser' => $frontendUser, 'settings' => $settings));
+	}
+}
+
+?>
