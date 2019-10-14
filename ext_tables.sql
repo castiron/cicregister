4171ce61b50704d643a76238e1b72f1072e68e13
Lucas Thurston <lucas@castironcoding.com>
2019-10-14 11:22:56 -0700

    [C] Fix fe_users SQL

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE fe_users (
 	tx_cicregister_sfdc_contact_id varchar(255) DEFAULT '' NOT NULL,
 	tx_cicregister_sfdc_lead_id varchar(255) DEFAULT '' NOT NULL,
-	tx_cicregister_sfdc_sync_timestamp varchar(255) DEFAULT '' NOT NULL
+	tx_cicregister_sfdc_sync_timestamp varchar(255) DEFAULT '' NOT NULL,
 	tx_cicregister_state varchar(255) DEFAULT '' NOT NULL,
 );
 
