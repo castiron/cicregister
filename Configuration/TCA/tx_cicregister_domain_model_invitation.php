f8b9d456f42a4fcd61bf31e94203a0bf78f6ad16
Kyle Pratuch <kyle@castironcoding.com>
2021-03-18 16:23:58 -0700

    [F] Fix TCA for v9

--- Configuration/TCA/tx_cicregister_domain_model_invitation.php
+++ Configuration/TCA/tx_cicregister_domain_model_invitation.php
@@ -3,13 +3,48 @@ if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
 
-$TCA['tx_cicregister_domain_model_invitation'] = array(
-	'ctrl' => $TCA['tx_cicregister_domain_model_invitation']['ctrl'],
+$tx_cicregister_domain_model_invitation = array(
+	'ctrl' => array(
+		'title'	=> 'LLL:EXT:cicregister/Resources/Private/Language/locallang_db.xml:tx_cicregister_domain_model_invitation',
+		'label' => 'email',
+		'tstamp' => 'tstamp',
+		'crdate' => 'crdate',
+		'cruser_id' => 'cruser_id',
+		'dividers2tabs' => TRUE,
+		'versioningWS' => 2,
+		'versioning_followPages' => TRUE,
+		'origUid' => 't3_origuid',
+		'languageField' => 'sys_language_uid',
+		'transOrigPointerField' => 'l10n_parent',
+		'transOrigDiffSourceField' => 'l10n_diffsource',
+		'delete' => 'deleted',
+		'searchFields' => 'email',
+		'enablecolumns' => array(
+			'disabled' => 'hidden',
+			'starttime' => 'starttime',
+			'endtime' => 'endtime',
+		),
+		'iconfile' => 'EXT:cicregister/Resources/Public/Icons/tx_cicregister_domain_model_invitation.gif'
+	),
 	'interface' => array(
 		'showRecordFieldList' => 'sys_language_uid, l10n_parent, l10n_diffsource, hidden, email, accepted, invited_by, expires_on, on_acceptance',
 	),
 	'types' => array(
-		'1' => array('showitem' => 'sys_language_uid;;;;1-1-1, l10n_parent, l10n_diffsource, hidden;;1, email, accepted, invited_by, expires_on, on_acceptance, --div--;LLL:EXT:cms/locallang_ttc.xml:tabs.access,starttime, endtime'),
+		'1' => array(
+			'showitem' =>
+				'sys_language_uid;LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.language, 
+				l10n_parent, 
+				l10n_diffsource, 
+				hidden;LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.hidden;1, 
+				email, 
+				accepted, 
+				invited_by, 
+				expires_on, 
+				on_acceptance, 
+				--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+				starttime;LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.starttime,
+				endtime;LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.endtime'
+		),
 	),
 	'palettes' => array(
 		'1' => array('showitem' => ''),
@@ -147,4 +182,4 @@ $TCA['tx_cicregister_domain_model_invitation'] = array(
 	),
 );
 
-?>
+return $tx_cicregister_domain_model_invitation;
